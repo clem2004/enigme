8f82e3f21cd5d1728746283f64248fef91b5cbfa
Clément Gauthier <57112933+clem2004@users.noreply.github.com>
2021-02-18 20:34:20 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ let input = document.getElementById('fname')
 input.onkeypress = function(e) {
     if ( e.key == 'Enter' ) {
         let url = 'https://clem2004.github.io/enigme/' + input.value.toLowerCase() + '.html'
+        window.open(url)
     }
 }
